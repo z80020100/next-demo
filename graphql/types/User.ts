d852f350a6a1e732d22545569507d6d16720bf44
Cliff Wu <z800201002005@gmail.com>
2022-06-07 13:10:16 +0900

    Define queries using Nexus

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -1,4 +1,4 @@
-import { enumType, objectType } from 'nexus'
+import { enumType, objectType, extendType } from 'nexus'
 
 export const User = objectType({
   name: 'User',
@@ -12,4 +12,16 @@ export const User = objectType({
 const Role = enumType({
   name: 'Role',
   members: ['USER', 'ADMIN'],
+})
+
+export const UsersQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('Users', {
+      type: 'User',
+      resolve(_parent, _args, ctx) {
+        return ctx.prisma.user.findMany()
+      },
+    })
+  },
 })
